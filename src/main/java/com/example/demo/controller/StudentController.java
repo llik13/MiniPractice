3a58b930d147ba62eb37dcc916f6cde95d8c60c8
llik13 <murchick2004@gmail.com>
2024-06-18 18:34:38 +0300

    add templates

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,4 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.StudentService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService){
+        super();
+        this.studentService = studentService;
+    }
+
+    //handler
+    @GetMapping("/students")
+    public String listStudent(Model model){
+        model.addAttribute("students", studentService.getAllStudents());
+        return "students";
+    }
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceImp.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImp.java
@@ -1,7 +1,24 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.Student;
+import com.example.demo.reprository.StudentRepository;
 import com.example.demo.service.StudentService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class StudentServiceImp implements StudentService {
+    private StudentRepository studentRepository;
 
+
+    public StudentServiceImp(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
 }
