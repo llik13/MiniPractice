d95c0ed4fb66965be216cd26e41fbd927970898a
llik13 <murchick2004@gmail.com>
2024-06-18 19:51:08 +0300

    create form(init)

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.StudentService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class StudentController {
     private StudentService studentService;
 
@@ -17,5 +19,9 @@ public class StudentController {
     public String listStudent(Model model){
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+
+    public String createStudentForm(){
+
     }
 }
